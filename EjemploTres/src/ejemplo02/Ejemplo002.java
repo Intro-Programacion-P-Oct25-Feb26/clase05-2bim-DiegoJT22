e742b9571abde2e0ecf38134be8b5260471f86f8
DiegoJT22 <daigoestrlar22@gmail.com>
2026-01-15 12:53:55 -0500

    Ultimo commit

--- EjemploTres/src/ejemplo02/Ejemplo002.java
+++ EjemploTres/src/ejemplo02/Ejemplo002.java
@@ -16,6 +16,7 @@ public class Ejemplo002 {
     public static void main(String[] args) {
         int [] valores = {10, 1, 3, 4, 9};
         boolean v = ArrayUtils.contains(valores, 9);
+
         
         System.out.printf("%s\n", v);
     }
